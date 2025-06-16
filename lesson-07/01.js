ff82d407690ee76816c64745d94dc5b92a5fd719
eevgeniiiiia <ev.sergeevna23@mail.ru>
2025-06-16 12:30:42 +0300

    Update 01.js

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,28 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+const img = document.getElementById("web-tech-image");
+const prevImg = document.getElementById("prev-button");
+const nextImg = document.getElementById("next-button");
+
+let indexImg = 0;
+
+img.src = WEB_TECH_IMAGES[indexImg];
+
+prevImg.addEventListener('click', function() {
+  indexImg = indexImg - 1;
+
+  if(indexImg < 0 ){
+    indexImg = WEB_TECH_IMAGES.length - 1;
+  }
+  img.src = WEB_TECH_IMAGES[indexImg];
+})
+
+
+nextImg.addEventListener('click', function() {
+  indexImg = indexImg + 1;
+  if(indexImg >= WEB_TECH_IMAGES.length  ){
+    indexImg = 0;
+  }
+  img.src = WEB_TECH_IMAGES[indexImg];
+})
